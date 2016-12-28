4b159ddf19769b232925c9f92b2e3cfe923e0729
kkiniaes <kkiniaes@gmail.com>
2016-12-28 13:13:50 +0530

    very simple start to movement and prototyping front end

---
+++ Assets/Scripts/MonoBehaviors/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	private Player player;
+	private Vector2 coordinate;
+	private Vector2 followPos;
+
+	public Movement followTarget;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(followTarget == null) {
+			Vector2 moveVector = Vector2.zero;
+			if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+				moveVector += Vector2.up;
+			}
+			if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+				moveVector += Vector2.left;
+			}
+			if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+				moveVector += Vector2.down;
+			}
+			if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+				moveVector += Vector2.right;
+			}
+
+			if (moveVector != Vector2.zero) {
+				transform.Translate(moveVector*Time.deltaTime*3f);
+				followPos = (Vector2)transform.position - moveVector/2f;
+			}
+	//		else {
+	//			transform.position = Vector3.MoveTowards(transform.position,
+	//				new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), 0f),
+	//				Time.deltaTime);
+	//		}
+		} else {
+			Vector3 newPos = Vector3.MoveTowards(transform.position, followTarget.followPos, Time.deltaTime*(1+Vector3.Distance(transform.position, followTarget.followPos)*2f));
+			if (newPos - transform.position != Vector3.zero) {
+				followPos = transform.position - (newPos - transform.position).normalized/2f;
+				transform.position = newPos;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MonoBehaviors/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Artifice.Characters;
+using UnityEngine;
+using System;
+
+public class Player : Entity {
+
+	[SerializeField]
+	private EntityInfo entityInfo;
+
+	// Use this for initialization
+	void Start () {
+
+		//TODO: This will be loaded in via XML reader later
+		this.title = entityInfo.name;
+		this.id = entityInfo.id;
+		this.stats = new CombatStats();
+		this.stats.Level = 1;
+		this.stats.BaseAccuracy = entityInfo.combatStats.accuracy;
+		this.stats.BaseDefense = entityInfo.combatStats.defense;
+		this.stats.BaseEvasion = entityInfo.combatStats.evasion;
+		this.stats.BaseMagic = entityInfo.combatStats.magic;
+		this.stats.BaseMagicDefense = entityInfo.combatStats.magicDefense;
+		this.stats.BaseMaxHealth = entityInfo.combatStats.maxHealth;
+		this.stats.BaseSpeed = entityInfo.combatStats.speed;
+		this.stats.BaseStrength = entityInfo.combatStats.strength;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	/// <summary>
+	/// A struct to hold necessary info for this entity,
+	/// allowing it to be easily accessed and modified
+	/// in the Unity Editor.
+	/// </summary>
+	[Serializable]
+	private struct EntityInfo {
+		public string name;
+		public string id;
+		public CombatStatsInfo combatStats;
+		public CharacterRace race;
+	}
+
+	/// <summary>
+	/// A struct to hold necessary info for this entity,
+	/// allowing it to be easily accessed and modified
+	/// in the Unity Editor.
+	/// </summary>
+	[Serializable]
+	private struct CombatStatsInfo {
+		[Range(1,20)]
+		public int strength, defense, magic, speed, evasion, accuracy, magicDefense, luck;
+
+		[Range(10,200)]
+		public int maxHealth;
+	}
+
+	#region implemented abstract members of Entity
+
+	public override void Interact ()
+	{
+		throw new NotImplementedException ();
+	}
+
+	public override void Die ()
+	{
+		Debug.Log(gameObject.name + " Died!");
+	}
+
+	#endregion
+
+}
