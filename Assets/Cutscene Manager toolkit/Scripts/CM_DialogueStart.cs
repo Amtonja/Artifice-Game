d1be45386c694df5a67bd791bf2f454ea11254fc
James <jamesroensley@gmail.com>
2017-04-18 14:40:55 -0400

    Light test work with the dialogue I need to push to combine with current build.

--- Assets/Cutscene Manager toolkit/Scripts/CM_DialogueStart.cs
+++ Assets/Cutscene Manager toolkit/Scripts/CM_DialogueStart.cs
@@ -1,20 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PixelCrushers.DialogueSystem;
 
 /// <summary>
-/// Stub for the dialogue integration with the CM chain.
+/// Dialogue integration with the CM chain.
 /// </summary>
 
 public class CM_DialogueStart : MonoBehaviour {
 
+	/// <summary>
+	/// The name of the dialogue.
+	/// </summary>
+	public string dialogueName; 
+
 	// Use this for initialization
 	void Start () {
-		
+		DialogueManager.StartConversation (dialogueName);
+//		DialogueManager.
+//		this.gameObject.SendMessage("DoThing");
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+//		if (DialogueManager. {
+//			PixelCrushers.DialogueSystem.SendMessageOnDialogueEvent(
+//			Debug.Log ("Convo over?");
+//		}
+	}
+
+
+	public void DoThing(){
+		UnityEngine.Debug.Log ("Batman!");
+
 	}
 }
