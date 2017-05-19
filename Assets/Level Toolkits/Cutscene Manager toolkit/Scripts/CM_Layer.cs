d81c5d1bf9cf8cf2bfa3ae615184121b3ecaf0d7
James <jamesroensley@gmail.com>
2017-05-19 16:43:31 -0400

    Elevator work
    Working on the elevator and systems related to making it work.

---
+++ Assets/Level Toolkits/Cutscene Manager toolkit/Scripts/CM_Layer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_Layer : MonoBehaviour {
+
+	//Changes the layer of a sprite. Used so you can, say, hide a sprite behind another.
+
+	public SpriteRenderer _sprite;
+	public string newLayer; //name of layer to change this sprite into
+
+	/// <summary>
+	/// The pass target. This is the next link in the CM chain.
+	/// </summary>
+	public GameObject passTarget;
+
+	// Use this for initialization
+	void Start () {
+//		_sprite.sortingLayerName = "VisualEffects";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Activate (){
+		_sprite.sortingLayerName = newLayer;
+		passTarget.SendMessage ("Activate");
+
+	}
+
+
+	void OnDrawGizmos(){
+		//	void OnDrawGizmosSelected(){
+		//		if(targetList != null){
+		if(passTarget != null){
+			//			foreach(GameObject target in targetList){
+
+			//draw a line from our position to it
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(this.transform.position, passTarget.transform.position);
+
+			//			}
+
+		}
+	}
+}
---
+++ Assets/Level Toolkits/Cutscene Manager toolkit/Scripts/CM_MoveObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_MoveObject : MonoBehaviour {
+
+	//Move multiple objects in a direction for a distance. Used mostly for non-player objects; does not account for animation.
+
+
+	public GameObject[] targets;
+	public Vector2 dir;
+	public float distance;
+
+
+	/// <summary>
+	/// The pass target. This is the next link in the CM chain.
+	/// </summary>
+	public GameObject passTarget;
+
+	private bool bRunning = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!bRunning) {
+			return;
+		}
+	}
+
+
+	void OnDrawGizmos(){
+		//	void OnDrawGizmosSelected(){
+		//		if(targetList != null){
+		if(passTarget != null){
+			//			foreach(GameObject target in targetList){
+
+			//draw a line from our position to it
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(this.transform.position, passTarget.transform.position);
+
+			//			}
+
+		}
+	}
+
+
+	void End(){
+		passTarget.SendMessage ("Activate");
+
+	}
+}
