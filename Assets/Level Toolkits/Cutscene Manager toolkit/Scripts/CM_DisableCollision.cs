dfa3121e45e5454f5cc020e4e4f72c89182c8a57
James <jamesroensley@gmail.com>
2017-06-12 15:37:49 -0400

    Relic Door update
    Door now has collision that is disabled when it is open.

---
+++ Assets/Level Toolkits/Cutscene Manager toolkit/Scripts/CM_DisableCollision.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_DisableCollision : MonoBehaviour {
+
+	//Disables an object's collision.
+
+	public BoxCollider2D target;
+
+	public GameObject passTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Called via CM modules to activate this script.
+	public void Activate(){
+		target.enabled = false;
+
+		passTarget.SendMessage ("Activate");
+	}
+
+
+	//For gizmo and editor debugging
+	void OnDrawGizmos(){
+		//	void OnDrawGizmosSelected(){
+		//		if(targetList != null){
+		if(passTarget != null){
+			//			foreach(GameObject target in targetList){
+
+			//draw a line from our position to it
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(this.transform.position, passTarget.transform.position);
+
+			//			}
+
+		}
+	}
+}
