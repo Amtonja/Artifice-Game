77c4d113f7557eedf245fffb4b3753f181cc1c16
James <jamesroensley@gmail.com>
2017-06-20 18:36:08 -0400

    Intro scene
    Added CM_LoadScene, which loads scenes.
    
    Title screen implimented.

---
+++ Assets/Level Toolkits/Cutscene Manager toolkit/Scripts/CM_LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_LoadScene : MonoBehaviour {
+
+
+	//Loads a new scene.
+	public string scene;
+
+	// Use this for initialization
+	void Activate(){
+		Application.LoadLevel (scene);
+	}
+}
