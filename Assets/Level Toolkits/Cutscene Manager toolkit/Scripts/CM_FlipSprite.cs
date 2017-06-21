13e0975bce409a032217c0f5a9aa6a92d02aeacc
James <jamesroensley@gmail.com>
2017-06-21 18:37:46 -0400

    CM_Flipsprite
    Flips a sprite horizontally.

---
+++ Assets/Level Toolkits/Cutscene Manager toolkit/Scripts/CM_FlipSprite.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_FlipSprite : MonoBehaviour {
+
+	//Flips a sprite horizontally.
+
+	public Transform target; //the target
+	// Use this for initialization
+
+	public bool bFlip = true; //turn false to revert
+
+	public GameObject passTarget;
+
+
+	public void Activate (){
+		if (bFlip) {
+			target.transform.localScale = new Vector3 (-2, 2, 1);
+		} else {
+			target.transform.localScale = new Vector3 (2, 2, 1);
+		}
+
+		passTarget.SendMessage ("Activate");
+
+	}
+
+
+	void OnDrawGizmos(){
+		//	void OnDrawGizmosSelected(){
+		//		if(targetList != null){
+		if(passTarget != null){
+			//			foreach(GameObject target in targetList){
+
+			//draw a line from our position to it
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(this.transform.position, passTarget.transform.position);
+
+			//			}
+
+		}
+
+	}
+}
