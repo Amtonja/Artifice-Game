cc77cf3363c3ce42acb6b25c57dc9cae64afb55f
James <jamesroensley@gmail.com>
2017-08-04 21:50:14 -0400

    Winter Leg Fix
    Fixed winter combat bug.
    
    Added CM_Splitter.
    
    Fixed incorrect aspect ratios being available on builds.

---
+++ Assets/Level Toolkits/Cutscene Manager toolkit/Scripts/CM_Splitter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM_Splitter : MonoBehaviour {
+
+
+	public GameObject[] passTarget;
+
+	public void Activate (){
+
+			foreach (GameObject target in passTarget) {
+				target.SendMessage ("Activate");
+
+				Debug.Log ("Splitter started!");
+			}
+
+
+
+	}
+
+
+
+	void OnDrawGizmos(){
+		//	void OnDrawGizmosSelected(){
+		//		if(targetList != null){
+		//		if(passTarget != null){
+		foreach(GameObject target in passTarget){
+
+			//draw a line from our position to it
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(this.transform.position, target.transform.position);
+
+			//			}
+
+		}
+	}
+}
