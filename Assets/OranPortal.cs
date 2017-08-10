1c416dc691881173d20a013dfa61419ef79e6c58
Alan Courchene <themajicou@gmail.com>
2017-08-09 22:01:00 -0400

    Restored elevator sound, portal visual and sound, wall exploding sound

---
+++ Assets/OranPortal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class OranPortal : MonoBehaviour
+{
+    private AudioSource _audio;
+    public AudioClip activationSound;
+    public AudioClip runningSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        StartCoroutine(PlayAudio());
+    }
+
+    IEnumerator PlayAudio()
+    {
+        _audio.PlayOneShot(activationSound);
+        yield return new WaitForSeconds(activationSound.length);
+        _audio.clip = runningSound;
+        _audio.loop = true;
+        _audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
