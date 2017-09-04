de326d2e6e04c6a3cf3083eb62f7ddfb312185be
Alan Courchene <themajicou@gmail.com>
2017-09-04 15:39:15 -0400

    Created simplest possible game save/load system

---
+++ Assets/SaveNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class SaveNode : MonoBehaviour
+{
+    public void Activate()
+    {
+        string saveGameData = PersistentDataManager.GetSaveData();
+        PlayManager.instance.OutputSaveGame(saveGameData);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
