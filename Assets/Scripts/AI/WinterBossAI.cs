bcdb6ef86fc8f74b60c1956f5e9b0f47307d10e7
James <jamesroensley@gmail.com>
2017-11-25 14:38:30 -0500

    Winter AI prototype
    Winter AI prototyped. On winter's turn, it passes a command to it's leg
    to attack.
    
    Requires gear and stats to be set up for it to be tested.

---
+++ Assets/Scripts/AI/WinterBossAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Artifice.Interfaces;
+
+namespace Artifice.Characters
+{
+
+	public class WinterBossAI : AIBase {
+
+		public GameObject leg; 
+		
+		public override void CombatUpdate()
+		{
+			if (_enemy.IsMyTurn) {
+				Debug.Log (gameObject.name.ToString () + "'s turn!");
+
+				//Cancel forcedMove
+				_movement.StopForcedMove (false);
+
+				leg.GetComponent<WinterBossLeg> ().LegAttack ();
+
+			}
+
+		}
+	}
+}
---
+++ Assets/Scripts/AI/WinterBossLeg.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Artifice.Interfaces;
+
+namespace Artifice.Characters
+{
+	public class WinterBossLeg : AIBase {
+
+		//Exists as an intermediary between the WinterBossAI and it's leg.
+
+	//	public void EndWeaponAttack(){
+	////		this.GetComponentInParent<Enemy> ().EndWeaponAttack ();
+	//
+	//	}
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+		//Called by WinterBossAI
+		public void LegAttack(){
+			Player person;
+
+			int partylength = PlayManager.instance.party.Length; //might need to be -1
+			//Characters[Mathf.RoundtoInt(Random.Range(Characters.length))}
+			person = PlayManager.instance.party[Mathf.RoundToInt(Random.Range(0,partylength))];
+
+			CombatEntity target = person.GetComponent<CombatEntity> ();
+
+			_enemy.MyCombatAction = _enemy.WeaponAttack;                    
+
+
+			Debug.Log ("Target = " + target.name.ToString ());
+			_enemy.ActiveWeapon = _enemy.GetComponent<Gear> ().primaryWeapon;
+			_enemy.MyCombatAction (target, _enemy.ActiveWeapon);
+
+
+		}
+	}
+}
